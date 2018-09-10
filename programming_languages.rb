f19d39904126d9d9681a6dc68b2dfeb9fa5f75b0
Nasser Sanou <nassersanou23@gmail.com>
2018-09-10 04:56:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,42 +1,41 @@
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
  
-  }
-}
-
+#   }
+# }
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |style, language|
+  languages.each do |style_def, language|
     language.each do |name , definition|
       new_hash[name] ||= definition
       new_hash[name][:style] ||= Array.new 
-      new_hash[name][:style] << style
+      new_hash[name][:style] << style_def
     end
   end
   new_hash
